db67a6a6f29a13b60689d2b7949e1349d1f9d0cd
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-24 22:25:35 +0900

    fin init example gradient_cached_image

---
+++ examples/gradient_cached_image/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:megu/megu.dart';
+
+import 'package:cached_network_image/cached_network_image.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        home: new Scaffold(
+            appBar: new AppBar(
+              brightness: Brightness.light,
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              elevation: 0.0,
+              title: new Text(
+                'Gradient Image',
+                style:
+                    TextStyle(color: Colors.grey, fontWeight: FontWeight.w300),
+              ),
+            ),
+            backgroundColor: Colors.white,
+            body: Center(
+              child: Container(
+                child: GradientCachedImage(
+                  height: 300,
+                  width: 300,
+                  startColor: Colors.lightBlue.withOpacity(0),
+                  endColor: Colors.blue.withOpacity(0.4),
+                  image: CachedNetworkImage(
+                    imageUrl: 'https://www.gstatic.com/webp/gallery3/1.png',
+                  ),
+                ),
+              ),
+            )));
+  }
+}
